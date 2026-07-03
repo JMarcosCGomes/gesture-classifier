e017f4819443c7e092bee47c5aca4907ce70f731
Joao Marcos <joaomarcoscgomes@gmail.com>
2026-07-02 23:36:41 -0300

    feat: add run_experiments.sh to run multiple experiments

---
+++ scripts/run_experiments.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+#DATASETS=("landmarks" "angles" "distances" "combined")
+DATASETS=("landmarks" "distances" "combined")
+MODELS=("linear" "onelayer" "twolayer")
+#TRAIN_FRACS=(0.1 0.25 0.5 1.0)
+TRAIN_FRACS=(0.1 1.0)
+DROPOUT=0.0
+
+total=$((${#DATASETS[@]} * ${#MODELS[@]} * ${#TRAIN_FRACS[@]}))
+count=0
+
+for train_frac in "${TRAIN_FRACS[@]}"; do
+  for dataset in "${DATASETS[@]}"; do
+    for model in "${MODELS[@]}"; do
+      count=$((count + 1))
+      echo "=== [$count/$total] dataset=$dataset model=$model train_frac=$train_frac ==="
+      python3 -m src.train --dataset "$dataset" --model "$model" --train-frac "$train_frac" --dropout $DROPOUT
+    done
+  done
+done
+
+echo "Todos os experimentos finalizados ($total runs)"
